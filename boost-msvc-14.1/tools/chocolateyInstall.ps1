802cf5c5d93ea0710ca503a029231041ddf67178
Jerome Bell <jeromebell@gmail.com>
2020-11-15 19:22:43 -0600

    Boost installs both 32 & 64 bit binaries; Boost uninstall script deletes entire release folder if no binaries are installed;

--- boost-msvc-14.1/tools/chocolateyInstall.ps1
+++ boost-msvc-14.1/tools/chocolateyInstall.ps1
@@ -1,10 +1,17 @@
+# Install 32 bit binaries
 Install-ChocolateyPackage `
-    -packageName 'boost-msvc14.1' `
+    -packageName 'boost-msvc-14.1' `
     -installerType 'exe' `
     -silentArgs '/VERYSILENT' `
     -url 'https://downloads.sourceforge.net/project/boost/boost-binaries/1.74.0/boost_1_74_0-msvc-14.1-32.exe' `
-    -url64bit 'https://downloads.sourceforge.net/project/boost/boost-binaries/1.74.0/boost_1_74_0-msvc-14.1-64.exe' `
 	-checksum 05b2681b181595e87c939954cf10593853193acb0f500b6af9119dd1615a2f25 `
 	-checksumType sha256 `
+
+# Install 64 bit binaries
+Install-ChocolateyPackage `
+    -packageName 'boost-msvc-14.1' `
+    -installerType 'exe' `
+    -silentArgs '/VERYSILENT' `
+    -url64bit 'https://downloads.sourceforge.net/project/boost/boost-binaries/1.74.0/boost_1_74_0-msvc-14.1-64.exe' `
 	-checksum64 81195a09449fb8d2611e4ecaa930acfc0404baae701bb166881ed9f0ad3886ad `
 	-checksumType64 sha256
--- boost-msvc-14.2/tools/chocolateyInstall.ps1
+++ boost-msvc-14.2/tools/chocolateyInstall.ps1
@@ -1,10 +1,17 @@
+# Install 32 bit binaries
 Install-ChocolateyPackage `
-    -packageName 'boost-msvc14.2' `
+    -packageName 'boost-msvc-14.2' `
     -installerType 'exe' `
     -silentArgs '/VERYSILENT' `
     -url 'https://downloads.sourceforge.net/project/boost/boost-binaries/1.74.0/boost_1_74_0-msvc-14.2-32.exe' `
-    -url64bit 'https://downloads.sourceforge.net/project/boost/boost-binaries/1.74.0/boost_1_74_0-msvc-14.2-64.exe' `
 	-checksum 529579fe4448f63f4e780338e4075c142604e40cd2c5901d5187160660b4ae48 `
 	-checksumType sha256 `
+
+# Install 64 bit binaries
+Install-ChocolateyPackage `
+    -packageName 'boost-msvc-14.2' `
+    -installerType 'exe' `
+    -silentArgs '/VERYSILENT' `
+    -url64bit 'https://downloads.sourceforge.net/project/boost/boost-binaries/1.74.0/boost_1_74_0-msvc-14.2-64.exe' `
 	-checksum64 74229a45b1e13f05c5bbdca7a268ac7723d97c33bec209e5f669b6cb3ffaeab6 `
 	-checksumType64 sha256
