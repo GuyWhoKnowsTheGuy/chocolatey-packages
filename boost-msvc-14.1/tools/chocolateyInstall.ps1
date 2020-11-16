459580c8f3dc6e577d7a39d38000c99de25dabcc
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2020-11-15 19:19:40 -0600

    Update boost to 1.74.0 (#2)

--- boost-msvc-14.1/tools/chocolateyInstall.ps1
+++ boost-msvc-14.1/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 Install-ChocolateyPackage `
-    -packageName 'boost-msvc12' `
+    -packageName 'boost-msvc14.1' `
     -installerType 'exe' `
     -silentArgs '/VERYSILENT' `
-    -url 'https://downloads.sourceforge.net/project/boost/boost-binaries/1.67.0/boost_1_67_0-msvc-14.1-32.exe' `
-    -url64bit 'https://downloads.sourceforge.net/project/boost/boost-binaries/1.67.0/boost_1_67_0-msvc-14.1-64.exe' `
-	-checksum 02ee7ba99e73f997daaba2e067a662f1c9a7d804dcf5890faa976fc7bff1a2e1 `
+    -url 'https://downloads.sourceforge.net/project/boost/boost-binaries/1.74.0/boost_1_74_0-msvc-14.1-32.exe' `
+    -url64bit 'https://downloads.sourceforge.net/project/boost/boost-binaries/1.74.0/boost_1_74_0-msvc-14.1-64.exe' `
+	-checksum 05b2681b181595e87c939954cf10593853193acb0f500b6af9119dd1615a2f25 `
 	-checksumType sha256 `
-	-checksum64 402d07022fe9671e401efc4e90a1ff25e1bc9e1c23b3d8b1c65e4a2e6799abfc `
+	-checksum64 81195a09449fb8d2611e4ecaa930acfc0404baae701bb166881ed9f0ad3886ad `
 	-checksumType64 sha256
